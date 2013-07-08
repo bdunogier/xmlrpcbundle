00042198ae9098166e18474861f8635cac89d888
Bertrand Dunogier <bd@ez.no>
2013-07-08 18:12:04 +0200

    Added parameters parsing to RequestParser

--- Tests/XmlRpc/RequestParserTest.php
+++ Tests/XmlRpc/RequestParserTest.php
@@ -11,6 +11,7 @@ namespace BD\Bundle\XmlRpcBundle\Tests\XmlRpc;
 
 use BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser;
 use PHPUnit_Framework_TestCase;
+use DateTime;
 
 class RequestParserTest extends PHPUnit_Framework_TestCase
 {
@@ -19,19 +20,57 @@ class RequestParserTest extends PHPUnit_Framework_TestCase
      * @covers RequestParser::__construct
      * @returns RequestParser The RequestParser with the loaded string
      */
-    public function testLoadXmLString()
+    public function testLoadXmlString( $xmlString = null )
     {
-        $xmlString = <<< XML
+        if ( !isset( $xmlString ) )
+        {
+            $xmlString = <<< XML
 <?xml version="1.0"?>
 <methodCall>
   <methodName>bdxmlrpc.getStuff</methodName>
   <params>
+    <param><value><i4>42</i4></value></param>
+    <param><value><int>84</int></value></param>
+
+    <param><value><string>Forty-two</string></value></param>
+    <param><value>Forty-two again</value></param>
+
+    <param><value><double>-42.21</double></value></param>
+
+    <param><value><dateTime.iso8601>20081219T20:01:00</dateTime.iso8601></value></param>
+
+    <param><value><base64>PD94bWwgdmVyc2lvbj0iMS4wIj8+CjxtZXRob2RDYWxsPgogIDxtZXRob2ROYW1lPmJkeG1scnBjLnRlc3Q8L21ldGhvZE5hbWU+CiAgPHBhcmFtcz4KICAgIDxwYXJhbT4KICAgICAgICA8dmFsdWU+PGk0PjQyPC9pND48L3ZhbHVlPgogICAgPC9wYXJhbT4KICA8L3BhcmFtcz4KPC9tZXRob2RDYWxsPgo=</base64></value></param>
+
     <param>
-        <value><i4>42</i4></value>
+        <value>
+            <struct>
+                <member>
+                    <name>int_element</name>
+                    <value><int>42</int></value>
+                </member>
+                <member>
+                    <name>string_element</name>
+                    <value>Forty-two</value>
+                </member>
+            </struct>
+        </value>
+    </param>
+
+    <param>
+        <value>
+            <array>
+                <data>
+                    <value><int>42</int></value>
+                    <value>Forty-two</value>
+                    <value><dateTime.iso8601>20081219T20:01:00</dateTime.iso8601></value>
+                </data>
+            </array>
+        </value>
     </param>
   </params>
 </methodCall>
 XML;
+        }
 
         $this->getRequestParser()->fromXmlString( $xmlString );
         return $this->getRequestParser();
@@ -65,12 +104,177 @@ XML;
     }
 
     /**
-     * @depends testLoadXmLString
+     * @depends testLoadXmlString
      * @param RequestParser $requestParser
      */
     public function testGetMethodName( RequestParser $requestParser)
     {
         self::assertEquals( 'bdxmlrpc.getStuff', $requestParser->getMethodName() );
+    }
+
+    /**
+     * @depends  testLoadXmlString
+     */
+    public function testGetArgumentsNoArguments()
+    {
+        $xmlString = <<< XML
+<?xml version="1.0"?>
+<methodCall>
+  <methodName>bdxmlrpc.testGetParametersNone</methodName>
+</methodCall>
+XML;
+        $parser = $this->testLoadXmLString( $xmlString );
+        self::assertEquals(
+            array(),
+            $parser->getParameters()
+        );
+    }
+
+    /**
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     * @depends  testLoadXmlString
+     */
+    public function testGetParametersNoneAgain()
+    {
+        $xmlString = <<< XML
+<?xml version="1.0"?>
+<methodCall>
+  <methodName>bdxmlrpc.testGetParametersNoArguments</methodName>
+  <params />
+</methodCall>
+XML;
+        $parser = $this->testLoadXmLString( $xmlString );
+        self::assertEquals(
+            array(),
+            $parser->getParameters()
+        );
+    }
+
+    /**
+     * @param RequestParser $requestParser A RequestParser instance with a loaded, valid XML
+     * @depends testLoadXmlString
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     * @return array Parameters from $requestParser
+     */
+    public function testGetParameters( RequestParser $requestParser )
+    {
+        self::assertInternalType( 'array', $requestParser->getParameters() );
+        return $requestParser->getParameters();
+    }
+
+    /**
+     * Tests <int> parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetIntegerParameter( $parameters )
+    {
+        self::assertEquals( 42, $parameters[0] );
+    }
+
+    /**
+     * Tests <i4> parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetI4Parameter( $parameters )
+    {
+        self::assertEquals( 84, $parameters[1] );
+    }
+
+    /**
+     * Tests <string> parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetStringParameter( $parameters )
+    {
+        self::assertEquals( "Forty-two", $parameters[2] );
+    }
+
+    /**
+     * Tests implicit string parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetImplicitStringParameter( $parameters )
+    {
+        self::assertEquals( "Forty-two again", $parameters[3] );
+    }
+
+    /**
+     * Tests double parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetDoubleParameter( $parameters )
+    {
+        self::assertEquals( -42.21, $parameters[4] );
+    }
+
+    /**
+     * Tests <dateTime.iso8601> parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetDateTimeParameter( $parameters )
+    {
+        self::assertEquals(
+            new DateTime( '2008-12-19 20:01:00' ),
+            $parameters[5]
+        );
+    }
+
+    /**
+     * Tests <base64> parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetBase64Parameter( $parameters )
+    {
+        self::assertEquals(
+            file_get_contents( realpath( __DIR__ . '/../../Resources/doc/xmlrpc/request.xml' ) ),
+            $parameters[6]
+        );
+    }
+
+    /**
+     * Tests <array> parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetStructParameter( $parameters )
+    {
+        self::assertEquals(
+            array( 'int_element' => 42, 'string_element' => 'Forty-two' ),
+            $parameters[7]
+        );
+    }
+
+    /**
+     * Tests <array> parameters
+     * @depends testGetParameters
+     * @param array Result from RequestParser::getParameters()
+     * @covers \BD\Bundle\XmlRpcBundle\XmlRpc\RequestParser::getParameters
+     */
+    public function testGetArrayParameter( $parameters )
+    {
+        self::assertEquals(
+            array(
+                42,
+                'Forty-two',
+                new DateTime( '2008-12-19 20:01:00' )
+            ),
+            $parameters[8]
+        );
     }
 
     /**
--- XmlRpc/RequestParser.php
+++ XmlRpc/RequestParser.php
@@ -9,12 +9,15 @@
 namespace BD\Bundle\XmlRpcBundle\XmlRpc;
 
 use SimpleXmlElement;
+use DateTime;
 
 /**
  * Parses an XML RPC request
  */
 class RequestParser
 {
+    const XML_RPC_ISO8601 = "Y#m#d\TH#i#s";
+
     /** @var \SimpleXMLElement */
     private $simpleXml;
 
@@ -39,6 +42,155 @@ class RequestParser
 
         if ( !isset( $this->simpleXml->methodName ) )
             throw new \UnexpectedValueException( "Invalid XML-RPC structure (/methodCall/methodName not found)" );
+    }
+
+
+    /**
+     * @return array()
+     */
+    public function getParameters()
+    {
+        if ( !isset( $this->simpleXml->params ) || $this->simpleXml->params->param->count() == 0 )
+            return array();
+
+        $parameters = array();
+        foreach ( $this->simpleXml->params->children() as $param )
+        {
+            if ( !isset( $param->value ) )
+            {
+                throw new \UnexpectedValueException( "Invalid <param> tag (no value)" );
+            }
+            $parameters[] = $this->processParameter( (array)$param->value );
+        }
+        return $parameters;
+    }
+
+    protected function processParameter( array $parameter )
+    {
+        $type = $this->getParameterType( key( $parameter ) );
+        $value = current( $parameter );
+
+        switch ( $type )
+        {
+            case 'int':
+                $value = (int)$value;
+                break;
+
+            case 'string':
+                $value = (string)$value;
+                break;
+
+            case 'double':
+                $value = (float)$value;
+                break;
+
+            case 'dateTime':
+                $value = $this->handleDateParameter( $value );
+                break;
+
+            case 'base64':
+                $value = base64_decode( (string)$value );
+                break;
+
+            case 'struct':
+                $value = $this->handleStructParameter( $value );
+                break;
+
+            case 'array':
+                $value = $this->handleArrayParameter( $value );
+                break;
+        }
+
+        return $value;
+    }
+
+    /**
+     * Parses a dateTime string. ISO-8601 is quite permissive as it makes the separators optional...
+     * What a *great* idea.
+     *
+     * @param string
+     *
+     * @return DateTime
+     */
+    protected function handleDateParameter( $value )
+    {
+        $date = DateTime::createFromFormat(
+            self::XML_RPC_ISO8601, (string)$value
+        );
+
+        //no separators, maybe, try again
+        if ( $date instanceof DateTime )
+            return $date;
+
+        $parts = explode( 'T', $value );
+
+        if ( count( $parts ) != 2 )
+            throw new \UnexpectedValueException( "Invalid date $value" );
+        if ( strlen( $parts[0] ) !== 8 || strlen( $parts[1] ) != 8)
+            throw new \UnexpectedValueException( "Invalid date $value" );
+
+        $dateParts = array();
+        sscanf( $parts[0], '%04d%02d%02d', $dateParts['year'], $dateParts['month'], $dateParts['day'] );
+        sscanf( $parts[1], '%02d:%02d:%02d', $dateParts['hours'], $dateParts['minutes'], $dateParts['seconds'] );
+        $iso8601date = sprintf(
+            '%4d-%02d-%02dT%02d:%02d:%02d',
+            $dateParts['year'], $dateParts['month'], $dateParts['day'],
+            $dateParts['hours'], $dateParts['minutes'], $dateParts['seconds']
+        );
+
+        return DateTime::createFromFormat( self::XML_RPC_ISO8601, $iso8601date );
+    }
+
+    /**
+     * @param $value
+     *
+     * @return array
+     */
+    protected function handleStructParameter( $value )
+    {
+        $values = array();
+        foreach ( $value->member as $value )
+        {
+            $values[(string)$value->name] = $this->processParameter( (array)$value->value );
+        }
+
+        return $values;
+    }
+
+    /**
+     * @param $value
+     *
+     * @return array
+     */
+    protected function handleArrayParameter( $value )
+    {
+        $values = array();
+        foreach ( $value->data->value as $value )
+        {
+            $values[] = $this->processParameter( (array)$value );
+        }
+
+        return $values;
+    }
+
+    protected function getParameterType( $typeIdentifier )
+    {
+        $map = array(
+            'i4' => 'int',
+            'int' => 'int',
+            'string' => 'string',
+            '0' => 'string',
+            'double' => 'double',
+            'dateTime.iso8601' => 'dateTime',
+            'base64' => 'base64',
+            'struct' => 'struct',
+            'array' => 'array'
+        );
+
+        if ( !isset( $map ) )
+            throw new \UnexpectedValueException( "Unknown parameter type '$typeIdentifier'" );
+
+        return $map[$typeIdentifier];
     }
 
     /**
